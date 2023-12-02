8e28d7d502dffc32b81db8f36aaeba8f8cc46c72
Romantik <romabludilin@yandex.ru>
2023-12-02 22:10:49 +0400

    WorkEnd1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
+function createCard (cardInfo, deleteCardCallBack){
+  const cardClone = cardTemplate.querySelector('.card').cloneNode(true);
+  cardClone.querySelector('.card__title').textContent = cardInfo.name;
+  cardClone.querySelector('.card__image').src = cardInfo.link;
 
-// @todo: Функция удаления карточки
+  const cardDeleteButton = cardClone.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', () => {deleteCardCallBack(cardClone);});
 
-// @todo: Вывести карточки на страницу
+  return cardClone;
+}
+
+function deleteCard (cardInfo) {
+  cardInfo.remove();
+}
+
+initialCards.forEach(item =>{
+  document.querySelector('.places__list').append(createCard(item, deleteCard));
+});
